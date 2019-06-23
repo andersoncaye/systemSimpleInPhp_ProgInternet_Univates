9640a93b3b93fc7ae915a639593b320fdd7ab415
Anderson Caye <andersoncaye@gmail.com>
2019-06-23 19:13:08 -0300

    page home

--- app/pages/home.php
+++ app/pages/home.php
@@ -13,14 +13,19 @@
     <div class="container">
         <div class="row">
             <div class="col bg-white">
-                <h2 class="bg-white" >Título Aqui</h2>
+
+                <br><br>
+
+                <h2 class="bg-white text-center" >CB Factory</h2>
+
+                <br>
+
+                <p class="text-center">GERENCIAMENTO DE CHEQUES RECEBIDOS</p>
 
 
-                    <p class="bg-white">Conteúdo Aqui</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut, ea, voluptatum voluptas veritatis optio voluptatibus laboriosam repudiandae recusandae ipsa sequi totam neque libero eum rerum, incidunt officiis fuga atque.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut, ea, voluptatum voluptas veritatis optio voluptatibus laboriosam repudiandae recusandae ipsa sequi totam neque libero eum rerum, incidunt officiis fuga atque.</p>
-                
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, eius aspernatur maiores fuga suscipit eveniet odit magnam eum, minus cum incidunt tempore minima nam id inventore debitis esse numquam fugit!</p>
+                <p class="text-center">
+                    <img src="assets/img/hero.jpg" style="width: 100%;">
+                </p>
 
             </div>
         </div>
